955fa5afcbb10a8e9507ca7c7783f6a07e736cd5
LEXES7 <sachinthabhashitha245@gmail.com>
2025-03-13 10:53:51 +0530

    connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+.connect( process.env.MONGO)
+    .then(() => {
+    console.log('Connected to MongoDB!!');
+    }
+)
+.catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 
 app.listen(8000, () => {
 
-    console.log('Server is running on port 8000!! ');
+    console.log('Server is running on port 8000');
     
     }
 );
