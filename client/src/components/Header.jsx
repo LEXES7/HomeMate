78d6324462edec8cc32542596947e912b5caa8de
LEXES7 <sachinthabhashitha245@gmail.com>
2025-03-18 04:40:12 +0530

    update header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import { Button, Navbar, NavbarCollapse, NavbarToggle, TextInput } from 'flowbite-react';
+import { Avatar, Button, Dropdown, Navbar, NavbarCollapse, NavbarToggle, TextInput } from 'flowbite-react';
 import React from 'react';
 import { Link, useLocation} from 'react-router-dom';
 import {AiOutlineSearch} from 'react-icons/ai';
 import {FaMoon} from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
     const path = useLocation().pathname;
+    const {currentUser} = useSelector(state => state.user)
   return (
     
 
@@ -29,14 +31,47 @@ export default function Header() {
         <Button className = 'w-12 h-10 hidden sm:inline' color ='gray' pill>
             <FaMoon/>
         </Button>
+
+        {currentUser ? (
+          <Dropdown
+            arrowIcon = {false}
+            inline
+            label ={
+              <Avatar
+              alt ='user'
+              img={currentUser.profilePicture}
+              />
+            }
+            >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+            <Dropdown.Item>profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider/>
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+
+        ):
+        (
+
         <Link to ='/signin'>
-        <Button gradientDuoTone='purpleToBlue' outline>
-            Sign In
+        <Button gradientDuoTone='purpleToBlue' outline> 
+          Sign In
         </Button>
         </Link>
-        <NavbarToggle/>
+
+
+        )
+
+}
+
+        
+        <Navbar.Toggle/>
         </div>
-        <NavbarCollapse>
+        <Navbar.Collapse>
             <Navbar.Link active={path === "/"} as={'div'}>
                 <Link to='/'>
                 Home
@@ -54,7 +89,7 @@ export default function Header() {
                 Features
                 </Link>
                 </Navbar.Link>
-            </NavbarCollapse>
+            </Navbar.Collapse>
     </Navbar>
     
   )
